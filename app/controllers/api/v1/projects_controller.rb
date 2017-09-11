d617aedb791264d5f5ecddfb089e5090c10cfbc3
ryskit <ryosukekida.71@gmail.com>
2017-09-11 21:17:54 +0900

    プロジェクトの削除機能を追加

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -8,12 +8,7 @@ class Api::V1::ProjectsController < AuthenticationController
     if project.save
       render json: { project: project }, status: :created
     else
-      render json: {
-        status: 'NG',
-        code: 400,
-        error: Rack::Utils::HTTP_STATUS_CODES[400],
-        messages: project.errors.messages
-      }, status: :bad_request
+      render_bad_request(project)
     end
   end
   
@@ -23,18 +18,33 @@ class Api::V1::ProjectsController < AuthenticationController
     if project && project.update_attributes(project_params)
       render json: { project: project }, status: :ok
     else
-      render json: {
-        status: 'NG',
-        code: 400,
-        error: Rack::Utils::HTTP_STATUS_CODES[400],
-        messages: project.errors.messages
-      }, status: :bad_request
+      render_bad_request(project)
     end
   end
-
+  
+  def destroy
+    project = Project.where("id = ? AND user_id = ?", params[:id], @user[:id]).first
+    
+    if project && project.destroy
+      render json: {}, status: :no_content
+    else
+      render_bad_request(project)
+    end
+  end
+  
   private
     
     def project_params
       params.require(:project).permit(:name)
+    end
+  
+    def render_bad_request(project = nil)
+      response_body = {
+        status: 'NG',
+        code: 400,
+        error: Rack::Utils::HTTP_STATUS_CODES[400],
+      }
+      response_body.merge!({ messages: project.errors.messages }) if project 
+      render json: response_body, status: :bad_request
     end
 end
--- spec/controllers/api/v1/projects_controller_spec.rb
+++ spec/controllers/api/v1/projects_controller_spec.rb
@@ -5,24 +5,29 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
   TASK_SIZE = 51
 
   before :each do
-    @user = create(:user)
-    payload = { :uuid => @user.uuid, :name => @user.name }
-    access_token = Token.create_access_token(payload)
+    users = create_list(:user, 3)
+    
+    # user
+    @user = users[0]
     refresh_token = @user.refresh_tokens.create
-
     @user_tokens = {
-      access_token: access_token,
+      access_token: Token.create_access_token({ :uuid => @user.uuid, :name => @user.name }),
       refresh_token: refresh_token.token,
       refresh_token_exp: refresh_token.expiration_at.to_i,
     }
     
-    
-    @tasks = []
-    TASK_SIZE.times{
-      task = @user.tasks.create(attributes_for(:update_task_attributes))
-      @tasks.push(task)
+    # another user
+    @another_user = users[1]
+    another_refresh_token = @another_user.refresh_tokens.create
+    @another_user_tokens = {
+      access_token: another_access_token = Token.create_access_token(
+        { :uuid => @another_user.uuid, :name => @another_user.name }
+      ),
+      refresh_token: another_refresh_token.token,
+      refresh_token_exp: another_refresh_token.expiration_at.to_i,
     }
-
+    
+    
     controller.request.headers['Authorization'] = "Bearer #{@user_tokens[:access_token]}"
     controller.request.headers['CONTENT_TYPE'] = 'application/json'
   end
@@ -79,6 +84,7 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
     
     before :each do
       @project = @user.projects.create(attributes_for(:valid_params))
+      @another_project = @another_user.projects.create(attributes_for(:valid_params))
     end
 
     let(:valid_params) { { project: attributes_for(:valid_update_params) } }
@@ -108,6 +114,17 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
           expect(res_body['error']).to eq Rack::Utils::HTTP_STATUS_CODES[400]
           expect(res_body['messages']['name'].present?).to be true
         end
+        
+        it '他プロジェクトの名前を更新しようとしてエラーとなる' do
+          expect do
+            patch :update, params: invalid_params.merge({ id: @another_project[:id] })
+          end.to change(Project, :count).by(0)
+          expect(response).to have_http_status(:bad_request)
+          res_body = JSON.parse(response.body)
+          expect(res_body['status']).to eq 'NG'
+          expect(res_body['code']).to eq 400
+          expect(res_body['error']).to eq Rack::Utils::HTTP_STATUS_CODES[400]
+        end
       end
     end
     
@@ -116,6 +133,65 @@ RSpec.describe Api::V1::ProjectsController, type: :controller do
         controller.request.headers['Authorization'] = "Bearer aaaaa"
         expect do
           patch :update, params: valid_params.merge({ id: @project[:id] })
+        end.to change(Project, :count).by(0)
+        expect(response).to have_http_status(:unauthorized)
+        res_body = JSON.parse(response.body)
+        expect(res_body['status']).to eq 'NG'
+        expect(res_body['code']).to eq 401
+        expect(res_body['error']).to eq Rack::Utils::HTTP_STATUS_CODES[401]
+      end
+    end
+  end
+  
+  describe 'DELETE#destroy' do
+    
+    before :each do
+      @project = @user.projects.create(attributes_for(:valid_params))
+      @another_project = @another_user.projects.create(attributes_for(:valid_params))
+    end
+
+    describe 'アクセストークンが有効な場合' do
+      context 'パラメータが有効な場合' do
+        it 'プロジェクトを削除する' do
+          expect do
+            delete :destroy, params: { id: @project[:id] }
+          end.to change(Project, :count).by(-1)
+          expect(response).to have_http_status(:no_content)
+          res_body = JSON.parse(response.body)
+          expect(res_body.blank?).to be true
+        end
+      end
+      
+      context 'パラメータが無効な場合' do
+        it '存在しないプロジェクトIDを指定してエラーとなる' do
+          expect do
+            patch :destroy, params: { id: 0 }
+          end.to change(Project, :count).by(0)
+          expect(response).to have_http_status(:bad_request)
+          res_body = JSON.parse(response.body)
+          expect(res_body['status']).to eq 'NG'
+          expect(res_body['code']).to eq 400
+          expect(res_body['error']).to eq Rack::Utils::HTTP_STATUS_CODES[400]
+        end
+        
+        it '他ユーザーのプロジェクトを削除できずにエラーとなる' do
+          expect do
+            patch :destroy, params: { id: @another_project.id }
+          end.to change(Project, :count).by(0)
+          expect(response).to have_http_status(:bad_request)
+          res_body = JSON.parse(response.body)
+          expect(res_body['status']).to eq 'NG'
+          expect(res_body['code']).to eq 400
+          expect(res_body['error']).to eq Rack::Utils::HTTP_STATUS_CODES[400]
+        end
+      end
+    end
+    
+    describe 'アクセストークンが無効な場合' do
+      it 'unauthorized errorとなる' do
+        controller.request.headers['Authorization'] = "Bearer aaaaa"
+        expect do
+          delete :destroy, params: { id: @project[:id] }
         end.to change(Project, :count).by(0)
         expect(response).to have_http_status(:unauthorized)
         res_body = JSON.parse(response.body)
